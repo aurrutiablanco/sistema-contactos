df5e7f0269067bd74b1ebdc92e10c557c8ba3cbd
aurrutiablanco <aurrutiablanco@gmail.com>
2026-06-12 16:48:24 -0400

    Add files via upload

---
+++ supabase-config.js
@@ -0,0 +1,4 @@
+// supabase-config.js
+const SUPABASE_URL = 'https://opzzmirnnydmzvpxgwdl.supabase.co';
+const SUPABASE_KEY = 'sb_publishable_h0uAJne1lnO1k-boxw5gRA_M0Ih2nW4';
+const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
